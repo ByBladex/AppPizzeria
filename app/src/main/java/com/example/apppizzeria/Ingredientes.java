25bbb436af2fb0841103ac05e6d107c5e2387dad
ByBladex <joselitojere@hotmail.com>
2021-01-27 11:09:58 +0100

    Diseño ingredientes

--- app/src/main/java/com/example/apppizzeria/Ingredientes.java
+++ app/src/main/java/com/example/apppizzeria/Ingredientes.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +15,10 @@ public class Ingredientes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredientes);
         Button btnConfirmar= findViewById(R.id.btnConfirmar);
+        RadioGroup radioGroupTamaño = findViewById(R.id.radioGroupTamaño);
+        RadioButton radioButtonPequeña = findViewById(R.id.radioButtonPequeña);
+        RadioButton radioButtonMediana = findViewById(R.id.radioButtonMediana);
+        RadioButton radioButtonFamiliar = findViewById(R.id.radioButtonFamiliar);
 
         btnConfirmar.setOnClickListener(new View.OnClickListener() {
             @Override
