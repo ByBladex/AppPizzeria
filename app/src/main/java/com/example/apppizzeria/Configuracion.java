0de54c28d9f0868b45c480a45516e793ba0d3f89
ByBladex <joselitojere@hotmail.com>
2021-02-02 11:51:35 +0100

    Prog Configuration

--- app/src/main/java/com/example/apppizzeria/Configuracion.java
+++ app/src/main/java/com/example/apppizzeria/Configuracion.java
@@ -1,13 +1,21 @@
 package com.example.apppizzeria;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class Configuracion extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracion);
+        Switch switchModOscuro = findViewById(R.id.switchModOscuro);
+        if(switchModOscuro.isActivated()){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        else
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 }
